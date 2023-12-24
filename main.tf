068d2c843e8333191b10d77e6b1fff1c5bd799f6
ich0103 <ich0103@naver.com>
2023-12-24 18:31:01 +0900

    add conf

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 terraform {
-  // cloud {
-  //   organization = "<MY_ORG_NAME>"         # 생성한 ORG 이름 지정
-  //   hostname     = "app.terraform.io"      # default
-
-  //   workspaces {
-  //     name = "collaboration"  # 없으면 생성됨
-  //   }
-  // }
+  cloud {
+    organization = "inchon-org"         # 생성한 ORG 이름 지정
+    hostname     = "app.terraform.io"      # default
+  workspaces {
+      name = "collaboration"  # 없으면 생성됨
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
